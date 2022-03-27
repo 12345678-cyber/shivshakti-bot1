ee29cac47356a24d2f6642e52ceaecb115400c10
Aman Pal <89698132+12345678-cyber@users.noreply.github.com>
2022-03-27 09:44:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "this bot is created by xeon, by javascript"
-echo "subscribe Xeon youtube channel for updates"
+echo "this bot is created by aman, by javascript"
+echo "you tube nhi hai git pebfollow kro"
 echo "All dependencies have been installed, please run the command \"(npm start)\" to immediately start the bot"
